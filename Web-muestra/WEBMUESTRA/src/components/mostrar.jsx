14c096e0589b6b6b2fb336fa18dc188f469f7b04
Mateo Benjamin Italiano De Francisca <italianobenja1@gmail.com>
2026-02-14 12:26:32 -0300

    CAMBIOS
    Cree carpeta styles dentro de src, para empezar a laburar con los estilos de toda la vida, a demas cree un archivo menu.css todo funcional ;)

---
+++ Web-muestra/WEBMUESTRA/src/components/mostrar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function menuARRIBA({ nombre, dueño }) {
+  return (
+    <section>
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </button>
+        <ul className="dropdown-menu">
+          <li><button className="dropdown-item" type="button">Action</button></li>
+          <li><button className="dropdown-item" type="button">Another action</button></li>
+          <li><button className="dropdown-item" type="button">Something else here</button></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default menuARRIBA;
