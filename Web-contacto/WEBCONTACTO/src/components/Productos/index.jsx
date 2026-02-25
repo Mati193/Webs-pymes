51b69d2dc0b3098a481e4ae4e67423518f3453b9
Mateo Benjamin Italiano De Francisca <italianobenja1@gmail.com>
2026-02-25 12:33:25 -0300

    Mejorando Estilos

--- Web-contacto/WEBCONTACTO/src/components/Productos/index.jsx
+++ Web-contacto/WEBCONTACTO/src/components/Productos/index.jsx
@@ -3,34 +3,34 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./productos.css";
 import "../global.css";
 
-// Esta funcion la hizo Mati
-// function Productos({ productos }) {
-//   return (
-//     <section className="fondo">
-//       <div className="titulos" id="productos">
-//         <h2>Nuestros productos</h2>
-//         <h5>Una seleccion de nuestras más deliciosas especialidades</h5>
-//       </div>
-//       <div className="contenedor-productos">
-//         {productos.map((producto, index) => (
-//           <div className="card card-hover" id={index}>
-//             <img
-//               src={producto[2]}
-//               className="img-productos"
-//               alt="Foto de algo rico"
-//             />
-//             <div className="card-body">
-//               <h5 className="card-title">{producto[0]}</h5>
-//               <p className="card-text">{producto[1]}</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
+/*Esta funcion la hizo Mati*/
+function Productos({ productos }) {
+  return (
+    <section className="fondo">
+      <div className="titulos" id="productos">
+        <h2>Nuestros productos</h2>
+        <h5>Una seleccion de nuestras más deliciosas especialidades</h5>
+      </div>
+      <div className="contenedor-productos">
+        {productos.map((producto, index) => (
+          <div className="card card-hover" id={index}>
+            <img
+              src={producto[2]}
+              className="img-productos"
+              alt="Foto de algo rico"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{producto[0]}</h5>
+              <p className="card-text">{producto[1]}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
 
-// Funcion de Tito
+/*Funcion de Tito
 function Productos({ productos }) {
   return (
     <section className="productos" id="productos">
@@ -48,5 +48,7 @@ function Productos({ productos }) {
     </section>
   );
 }
+
+*/
 
 export default Productos;
