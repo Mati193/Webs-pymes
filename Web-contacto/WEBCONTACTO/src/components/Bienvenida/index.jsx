6bd54e6f99fbcf40acd5f36327c7506055dc0fff
Matías Sposetti <matisposetti@mi.unc.edu.ar>
2026-03-07 13:18:26 -0300

    Agregados algunos efectos con la libreria AOS

--- Web-contacto/WEBCONTACTO/src/components/Bienvenida/index.jsx
+++ Web-contacto/WEBCONTACTO/src/components/Bienvenida/index.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { motion } from "motion/react";
 import Boton from "../Boton/index";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./bienvenida.css";
 import "../global.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function Bienvenida({ logo, marca, eslogan, t1, desc }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // duración de animación
+      once: true, // solo se anima una vez
+    });
+  }, []);
+
   return (
     <div className="parent" id="bienvenida">
-      <span className="section-badge">{t1}</span>
+      <span className="section-badge" data-aos="fade-down">
+        {t1}
+      </span>
 
-      <h2 className="resaltado">{marca}</h2>
-      <img src={logo} alt="logo" className="logo rounded-circle img-fluid" />
+      <h2 className="resaltado" data-aos="fade-right">
+        {marca}
+      </h2>
+      <img
+        src={logo}
+        alt="logo"
+        className="logo rounded-circle img-fluid"
+        data-aos="fade-up-right"
+      />
 
-      <h4>{eslogan}</h4>
+      <h4 data-aos="fade-right">{eslogan}</h4>
 
-      <h5>{desc}</h5>
+      <h5 data-aos="fade-down">{desc}</h5>
       <div className="botonera">
-        <a href="#productos">
+        <a href="#productos" data-aos="fade-right">
           <Boton texto={"Nuestros productos"} tipo={"claro"} />
         </a>
-        <a href="#contacto">
+        <a href="#contacto" data-aos="fade-left">
           <Boton texto={"Pedir ahora!"} tipo={"oscuro"} />
         </a>
       </div>
